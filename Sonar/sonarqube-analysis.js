6cc127236e04e213dc1ff04755ab0f697bfe760a
Mounir Missaoui <Mounir@fivepoints.fr>
2019-10-07 14:54:47 +0100

    added sonarQube Quality Assessment

---
+++ Sonar/sonarqube-analysis.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var sonarqubeScanner = require('sonarqube-scanner');
+
+gulp.task('default', function (callback) {
+    sonarqubeScanner({
+        serverUrl: "http://oursonarhero.fivepoints.fr",
+        token: "10aae15fc10e65ecb3bd59835de1f45ef99e6bfd",
+    }, callback);
+});

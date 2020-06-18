d1f4506a354c4665deccd2c13cb6184ece7c9bc6
Chehir Dhawedi <abe.crunck@gmail.com>
2020-06-18 15:48:59 +0000

    Update src/app.module.ts, src/app.controller.ts files

---
+++ src/app.controller.ts
@@ -0,0 +1,19 @@
+
+import { Controller, Get, Post, Body } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  constructor() {}
+
+  @Get()
+  getHello() {
+      return {message:'OK'};
+    // return this.appService.getHello();
+  }
+  @Post('payment')
+  paymentNotif(@Body() body:any){
+    console.log('hello');
+    console.log(body);
+    return {message:'NOT OK'};
+  }
+}
